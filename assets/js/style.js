ca914fb325cc9d9615f493006b17c5fdf7060579
Makka <142877128+maylova93@users.noreply.github.com>
2024-03-01 12:51:27 +0100

    Update style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,177 +1,260 @@
+
 // globals
-const myFeaturedElement = document.getElementById('featuredProducts');
+const productSection = document.getElementById('featuredProducts');
 const navElement = document.getElementById('navigation');
 
+
+
 let myProducts = null
+
 
 // page load
 GetProductData()
 GetCategoryData()
 
 
-// model
-function GetProductData() {
-    fetch('https://dummyjson.com/products?limit=100')
-        .then((result) => {
+/* Model code------------------------------------------------------------- */
 
-            return result.json();
-        })
+function GetProductData() {
+
+    fetch('https://dummyjson.com/products?limit=100')
+
+        .then((result) => {
+            //console.log(result);
+            return result.json()
+        }
+        )
+
         .then((json) => {
+            console.log(json);
             ProductsRecived(json)
         });
 }
 
-function GetProductsByCategory(myCategoryURL){
-    fetch(myCategoryURL)
-    .then((result) => {
 
-        return result.json();
-    })
-    .then((json) => {
-        ProductsRecived(json)
-    });
+function GetProductsByCategory(myCategoryURL) {
+
+    fetch(myCategoryURL)
+
+        .then((result) => {
+            //console.log(result);
+            return result.json()
+        }
+        )
+
+        .then((json) => {
+
+
+
+            recivedProductsByCategory(json)
+        });
+
 }
 
 
-
-// NAVIGATION 
 function GetCategoryData() {
-    
+
     fetch('https://dummyjson.com/products/categories')
 
         .then((result) => {
             return result.json()
-        })
+        }
+        )
 
         .then((json) => {
+            //console.log(json);
             CategoryRecived(json)
         });
 }
 
-/////CONTROLLER CODE
-function recivedProductsByCategory(productsByC){
-    let myProductArray = productsByC.product
-    CreateProductView( productdata.products)
+
+
+
+
+/* controller code------------------------------------------------------------- */
+
+function recivedProductsByCategory(productsByC) {
+
+    let myProductArray = productsByC.products
+
+    CreateProductView(myProductArray)
 
 }
 
 
 
+function CategoryRecived(CategoryData) {
+    // skriv lækker kode der kan sortere kategorier her.. nu sender vi bare alt videre.
+    // console.log(CategoryData);
 
-function CategoryRecived(categoryData){
-CreateNavBar(categoryData)
-
+    CreateNavBar(CategoryData)
 }
 
-
-
-// controller
+//----------------------------------------------------------------------
 function ProductsRecived(productData) {
 
     //console.log(productData)
-
-    let myProducts = productData.products
-
 
     myProducts = productData.products
 
     let myFeaturedProducts = [];
 
-    myFeaturedProducts.push(myProducts[6], myProducts[9], myProducts[23])
-
-    myFeaturedProducts.push(myProducts[12], myProducts[29], myProducts[19])
+    myFeaturedProducts.push(myProducts[8], myProducts[29], myProducts[19])
     //console.log(myFeaturedProducts);
 
     CreateProductView(myFeaturedProducts)
+    // CreateProductView(myProducts)
+}
 
-    //CreateProductView(myProducts)
+//----------------------------------------------------------------------
+
+function NavCallback(CategoryName) {
+    console.log(CategoryName);
+    /*   // vi har Data
+  
+      let myCategoryProducts = []
+  
+      myProducts.forEach(product => {
+          if (product.category == CategoryName) {
+              myCategoryProducts.push(product)
+          }
+      });
+      
+    CreateProductView(myCategoryProducts)
+     */
+
+
+    // get data from API  bug API url og send videre
+    let myCategoryURL = `https://dummyjson.com/products/category/${CategoryName}`
+
+    GetProductsByCategory(myCategoryURL)
+
+
+
+
+}
+
+//
+
+
+
+//----------------------------------------------------------------------
+function ProductCallback(myId) {
+
+
+
+    console.log(myId);
+    let myClickedProduct = null
+
+
+    myProducts.forEach(product => {
+
+        if (product.id == myId) {
+            myClickedProduct = product
+        }
+    }
+    )
+
+    if (myClickedProduct == null) {
+        // ingen produkt
+        alert('no product')
+    }
+    else {
+        // produkt
+        console.log(myClickedProduct)
+        clearApp();
+        buildProduct(myClickedProduct)
+
+    }
+
 }
 
 
+/* view code------------------------------------------------------------- */
 
+function CreateNavBar(Categorydata) {
 
-function NavCallBack(CategoryName){
-console.log(CategoryName);
-// vi har data
+    let myNavHTML = ""
 
-// let myCategoryProducts=[]
+    Categorydata.forEach(categoryName => {
 
-
-// myProducts.forEach(product => {
-//     if (product.category == CategoryName) {
-//          myCategoryProducts.push(product)
-//     }
-// });
-let myCategoryURL =`https://dummyjson.com/products/category/smartphones/${CategoryName}`
-GetProductsByCategory(myCategoryURL)
-}
-
-
-
-// view code......................
-function CreateNavBar(categoryData){
-    let myNavHTML= ""
-    
-    categoryData.forEach(CategoryName => {
-     
-        let myButton=`<button  onclick="NavCallBack(${CategoryName}) >${CategoryName}</button>` 
+        let myButton = `<button onclick="NavCallback('${categoryName}')" >${categoryName}</button>`
         myNavHTML += myButton
-
     });
-    navElement .innerHTML=myNavHTML
+
+    navElement.innerHTML = myNavHTML
+
 }
 
-
-
-
-
-
-
-
-// view code
+//----------------------------------------------------------------------
 function CreateProductView(myCards) {
-    console.log(myCards);
     //console.log(myCards);
+    clearApp()
 
     myCards.forEach(product => {
-        console.log(product);
+        // console.log(product);
 
 
         let myHTML = `<figure onclick="ProductCallback(${product.id})" ><h2>${product.title}</h2><img src="${product.thumbnail}"><h3>PRIS: ${product.price} rabat: ${product.discountPercentage}</h3></figure>`
 
 
-        myFeaturedElement.innerHTML += myHTML
-    });
+        productSection.innerHTML += myHTML
+    })
 }
 
 
+//----------------------------------------------------------------------
+function buildProduct(product) {
+
+    let myHTML = `<figure class="productDetails"><h2>${product.title}</h2>
+  
+    <img src="${product.images[0]}">
+    <img src="${product.images[2]}">
+    <img src="${product.images[3]}">
+    <h3>PRIS: ${product.price}</h3>
+    <p>${product.description}</p>
+    <button onclick="pruductcard(${product.id})" >Add to card</button>
+    </figure>
+    `
 
 
-function ProductCallback(myId) {
+    productSection.innerHTML = myHTML
+}
+
+
+//----------------------------------------------------------------------
+function clearApp() {
+    productSection.innerHTML = ""
+}
+
+function pruductcard(myId) {
+
+
+
     console.log(myId);
-    let myClickedProduct = null;
+    let myClickedProduct = null
+
+
     myProducts.forEach(product => {
+
         if (product.id == myId) {
-            myClickedProduct = product;
+            myClickedProduct = product
         }
     }
     )
-    console.log(myClickedProduct);
-    clearApp();
-    buildProduct(myClickedProduct)
-}
 
-function buildProduct(product) {
+    if (myClickedProduct == null) {
+        // ingen produkt
+        alert('no product')
+    }
+    else {
+        // produkt
+        console.log(myClickedProduct)
+        clearApp();
+        buildProduct(myClickedProduct)
 
-    let myHTML = `<figure onclick="ProductCallback(${product.id})" ><h2>${product.title}</h2><img src="${product.thumbnail}"><h3>PRIS: ${product.price}</h3></figure>`
-
-
-    myFeaturedElement.innerHTML = myHTML
-}
-
-function clearApp() {
+    }
 
 }
-
 
 
